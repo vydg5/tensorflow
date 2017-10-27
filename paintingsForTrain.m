a0ffd28206249d3889ebed0f8d3906a97a295cfa
Vireak Yim <vydg5@mail.missouri.edu>
2017-10-27 15:42:12 -0500

    extracting and labeling the paintings

--- paintingsForTrain.m
+++ paintingsForTrain.m
@@ -1,9 +1,29 @@
 % Vireak Yim
 % Move paintings from the train folder to another folder
-source_folder = '/Users/vireakyim/Desktop/3rd Year/SupLearning/Project/supervised_Project';
-target_folder = '/Users/vireakyim/Desktop/paintingsForTrain';
-for i=1:3
-    j = 2;
-    file = sprintf('Organization%f',num2str(j) ,'.docx');
-    copyfile('origine',file);
+tic
+cd C:\Users\vydg5\Desktop\Deep-NN\SupervisedLearning\train\train;
+target_folder = 'C:\Users\vydg5\Desktop\paintingsTraining_Original';
+label_temp = cell(1,length(index_BGW));
+k = 1;
+for i=1:length(index_BGW)
+    paintingId = [num2str(index_BGW(i)) ,'.jpg'];
+    if exist(paintingId, 'file')
+        copyfile(paintingId, target_folder);
+        if index_BGW(i) < 341 && index_BGW(i) > 126
+            label_temp{k} = 'Wassily';
+            k = k+1;
+        end
+         if index_BGW(i) < 1842 && index_BGW(i) > 1519
+            label_temp{k} = 'Bierstadt';
+            k = k+1;
+         end
+         if index_BGW(i) < 98267 && index_BGW(i) > 97776
+            label_temp{k} = 'Gogh';
+            k = k+1;
+        end
+    else continue
+    end
 end
+label = label_temp(1,1:k-1);
+clear i k labeltemp target_folder paintingId;
+toc
